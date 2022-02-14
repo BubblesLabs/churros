a91f3a7395adfc74440e613bcee3f1ffc3f13564
Bubbles Labs <99173462+BubblesLabs@users.noreply.github.com>
2022-02-14 18:09:22 +0000

    Added Github logo and link

--- src/components/Footer/Footer.svelte
+++ src/components/Footer/Footer.svelte
@@ -1,13 +1,33 @@
 <style lang="scss">
   footer {
+    padding: 0px 20px;
     font-size: 0.7rem;
     text-align: center;
     display: flex;
-    justify-content: center;
+    justify-content: space-between;
     align-items: center;
+
+    a {
+      text-decoration: none;
+      color: inherit;
+    }
+
+    img {
+      height: 18px;
+    }
   }
 </style>
 
 <footer>
+  <div />
   <div>Made by © Bubbles Labs 2022</div>
+  <div>
+    <a
+      href="https://github.com/BubblesLabs/churros"
+      target="_blank"
+      rel="noopener noreferrer nofollow"
+    >
+      <img src="img/github.png" alt="github" />
+    </a>
+  </div>
 </footer>
